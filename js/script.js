52df31a0e3649b3138b81cf75da178524c32cb2f
Anderey Prokhorov <beta737@yandex.ru>
2021-12-15 20:44:55 +0300

    Исправил 15 урок

--- js/script.js
+++ js/script.js
@@ -1,58 +1,71 @@
 'use strict';
 
+document.addEventListener("DOMContentLoaded", () => {
+    const DomElement = function (selector, height, width, bg, fontSize) {
+        this.selector = selector,
+            this.height = height,
+            this.width = width,
+            this.bg = bg,
+            this.fontSize = fontSize
 
 
-const DomElement = function (selector, height, width, bg, fontSize) {
-    this.selector = selector,
-        this.height = height,
-        this.width = width,
-        this.bg = bg,
-        this.fontSize = fontSize
+    };
+    DomElement.prototype.createElem = function () {
 
 
-};
-DomElement.prototype.createElem = function () {
+        if (this.selector[0] === '.') {
+            const div = document.createElement('div');
+            div.classList.add(this.selector);
+            div.style.cssText = `
+                height: ${this.height};
+                width: ${this.width};
+                background: ${this.bg};
+                font-size: ${this.fontSize};
+                margin: 0 auto; 
+                `;
+            div.innerHTML = 'Это созданный DIV';
+            document.body.append(div);
+        }
+        if (this.selector[0] === '#') {
+            const p = document.createElement('p');
+            p.id = this.selector;
+            p.style.cssText = `
+                height: ${this.height};
+                width: ${this.width};
+                background: ${this.bg};
+                font-size: ${this.fontSize}; 
+                margin: 0 auto;
+            `;
+            p.innerHTML = 'Это созданный P';
+            document.body.append(p);
 
-   
-    if (this.selector[0] === '.') {
+        }
+
+    };
+    DomElement.prototype.createSquare = function () {
         const div = document.createElement('div');
         div.style.cssText = `
+            position: absolute;
             height: ${this.height};
             width: ${this.width};
             background: ${this.bg};
-            font-size: ${this.fontSize};
-            margin: 0 auto; 
             `;
-        div.innerHTML = 'Это созданный DIV';
         document.body.append(div);
     }
-    if (this.selector[0] === '#') {
-        const p = document.createElement('p');
-        p.style.cssText = `
-            height: ${this.height};
-            width: ${this.width};
-            background: ${this.bg};
-            font-size: ${this.fontSize}; 
-            margin: 0 auto;
-           `;
-        p.innerHTML = 'Это созданный P';
-        document.body.append(p);
 
-    }
+    const domElem1 = new DomElement('.block', '400px', '400px', 'yellow', '60px');
 
-};
+    const domElem2 = new DomElement('#main', '300px', '300px', 'blue', '30px');
 
+    domElem1.createElem();
+    domElem2.createElem();
 
-const domElem1 = new DomElement('.block', '400px', '400px', 'yellow', '60px');
+    // document.addEventListener('DomContentLoaded', (е) => {
 
-const domElem2 = new DomElement('#main', '300px', '300px', 'blue', '30px');
+    const square = new DomElement('.block', '100px', '100px', 'grey');
+    square.createSquare();
+});
 
 
 
-domElem1.createElem();
-domElem2.createElem();
-
-console.log(domElem1);
-
-
-
+// }); 
